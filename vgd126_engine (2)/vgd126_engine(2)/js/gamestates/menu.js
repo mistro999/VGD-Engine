8eed3a9a21c7f5ae499dfd8df17ecc49af2cdcda
mistro999 <nrpranami@email.neit.edu>
2026-09-10 18:01:30 -0400

    trying to fix background sound

--- vgd126_engine (2)/vgd126_engine(2)/js/gamestates/menu.js
+++ vgd126_engine (2)/vgd126_engine(2)/js/gamestates/menu.js
@@ -20,7 +20,7 @@ menuBackground.height = canvas.height;
 gameStates[`menu`] = function () {
   // Hover & click
   if (startButton.overlap(mouse)) {
-    if (mouse.pressed) { gameStates.changeState(`level1`)  *sounds.play(`jump`,loop = true); }
+    if (mouse.pressed) { gameStates.changeState(`level1`); sounds.play(`Ashfall`, 0, loop = true); }
     startButton.color = `yellow`;} else {
     startButton.color = `red`;
   }
