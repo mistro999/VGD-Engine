1a71fafd8bfce31af8aa65d9426825a741987ec0
mistro999 <nrpranami@email.neit.edu>
2026-09-10 17:03:34 -0400

    sound added

--- vgd126_engine (2)/vgd126_engine(2)/js/classes/SoundManager.js
+++ vgd126_engine (2)/vgd126_engine(2)/js/classes/SoundManager.js
@@ -29,4 +29,4 @@ class SoundManager {
 let soundNodes = document.querySelectorAll(`audio`)
 if (soundNodes.length > 0) var sounds = new SoundManager(soundNodes)
 soundNodes = null
-document.addEventListener(`click`, () => Ashfall.play(`Ashfall`, .5))
+//document.addEventListener(`click`, () => sound.play(`Ashfall`, .5))
