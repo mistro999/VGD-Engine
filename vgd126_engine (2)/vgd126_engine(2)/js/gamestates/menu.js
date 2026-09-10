64c473044fbb8dde586ef07bf39ad6bbf226c5df
mistro999 <nrpranami@email.neit.edu>
2026-09-10 17:12:57 -0400

    fixed jump sound

--- vgd126_engine (2)/vgd126_engine(2)/js/gamestates/menu.js
+++ vgd126_engine (2)/vgd126_engine(2)/js/gamestates/menu.js
@@ -20,7 +20,7 @@ menuBackground.height = canvas.height;
 gameStates[`menu`] = function () {
   // Hover & click
   if (startButton.overlap(mouse)) {
-    if (mouse.pressed) { gameStates.changeState(`level1`)  /*sounds.play(`jump`,loop = true)*/; }
+    if (mouse.pressed) { gameStates.changeState(`level1`)  *sounds.play(`jump`,loop = true)*; }
     startButton.color = `yellow`;
   } else {
     startButton.color = `red`;
